354a30780b5644426b43c7021bbc3fa859519f82
Joel Domínguez <joel.dominguez@itequia.com>
2024-12-18 15:49:57 +0100

    WIP

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import { EntrenamientoComponent } from './entrenamiento/entrenamiento.component';
-import { EjerciciosComponent } from './ejercicios/ejercicios.component';
-import { EjercicioDetailsComponent } from './ejercicio-details/ejercicio-details.component';
+import { EntrenamientoComponent } from './pages/entrenamiento/entrenamiento.component';
+import { EjerciciosComponent } from './pages/ejercicios/ejercicios.component';
+import { EjercicioDetailsComponent } from './pages/ejercicio-details/ejercicio-details.component';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
-    { path: '', component: EntrenamientoComponent },
+    { path: '', component: HomeComponent },
+    { path: 'entrenamientos', component: EntrenamientoComponent },
     { path: 'ejercicios', component: EjerciciosComponent },
     { path: 'ejercicios/:ejercicioId', component: EjercicioDetailsComponent }
 ];
